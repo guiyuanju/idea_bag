b07728c909069bf8aad034e5db845674433c6208
guiyuanju <guiyuanjune@outlook.com>
2025-06-25 15:14:10 +0800

    init

---
+++ Design.md
@@ -0,0 +1,25 @@
+# Design Note
+
+project idea & languages/technique
+
+practicing or explore new language/framework/technique by doing projects.
+
+provide a idea bag.
+
+Project: string
+  - Blog Generator
+  - AirTable
+
+Tag: string
+  - Dev Tool
+  - Web
+  - Network
+  - Game
+
+Tool: string
+  - Rust
+  - Go
+  - Java
+
+format: csv
+name,"#tag1#tag2","&tool1&tool2"
---
+++ csv.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+func toCsv(entries []Entry, writer io.Writer) error {
+	header := []byte("project,tags,tools\n")
+	_, err := writer.Write(header)
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		line := []byte(entry.project)
+		line = append(line, byte(','))
+
+		for _, tag := range entry.tags {
+			line = append(line, []byte(tag)...)
+		}
+		line = append(line, byte(','))
+
+		for _, tool := range entry.tools {
+			line = append(line, []byte(tool)...)
+		}
+		line = append(line, byte('\n'))
+
+		_, err := writer.Write(line)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func fromCsv(reader io.Reader) []Entry {
+	var res []Entry
+	scanner := bufio.NewScanner(reader)
+	scanner.Scan() // jump the head
+	for scanner.Scan() {
+		entry := Entry{}
+		line := scanner.Text()
+		fields := strings.Split(line, ",")
+		entry.project = fields[0]
+
+		var tags []string
+		curTag := ""
+		for _, c := range fields[1] {
+			if c == '#' {
+				if len(curTag) > 1 {
+					tags = append(tags, curTag)
+				}
+				curTag = string(c)
+			} else {
+				curTag += string(c)
+			}
+		}
+		if len(curTag) > 1 {
+			tags = append(tags, curTag)
+		}
+		entry.tags = tags
+
+		var tools []string
+		curTool := ""
+		for _, c := range fields[2] {
+			if c == '&' {
+				if len(curTool) > 1 {
+					tools = append(tools, curTool)
+				}
+				curTool = string(c)
+			} else {
+				curTool += string(c)
+			}
+		}
+		if len(curTool) > 1 {
+			tools = append(tools, curTool)
+		}
+		entry.tools = tools
+
+		res = append(res, entry)
+	}
+
+	return res
+}
